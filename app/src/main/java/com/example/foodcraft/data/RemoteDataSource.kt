8cccf3da60e137fcc109f88f913649ecafbdc65c
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-01 03:27:26 +0530

    Implement RemoteDataSource

---
+++ app/src/main/java/com/example/foodcraft/data/RemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.example.foodcraft.data
+
+import com.example.foodcraft.data.network.FoodRecipesApi
+import com.example.foodcraft.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>) : Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
+}
