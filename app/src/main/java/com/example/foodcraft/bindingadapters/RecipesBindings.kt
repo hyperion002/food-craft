5fb65c543a0d37c4feaf9159f6845e06c03653ec
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-01 03:26:39 +0530

    Implement RecipesBindings.kt

---
+++ app/src/main/java/com/example/foodcraft/bindingadapters/RecipesBindings.kt
@@ -0,0 +1,49 @@
+package com.example.foodcraft.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.foodcraft.data.database.RecipesEntity
+import com.example.foodcraft.models.FoodRecipe
+import com.example.foodcraft.util.NetworkResult
+
+class RecipesBindings {
+
+    companion object {
+
+        @BindingAdapter("readApiResponseForImage", "readDatabaseForImage", requireAll = true)
+        @JvmStatic
+        fun errorImageViewVisibility(
+            imageView: ImageView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                imageView.visibility = View.VISIBLE
+            } else if (apiResponse is NetworkResult.Loading) {
+                imageView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+
+        @BindingAdapter("readApiResponseForText", "readDatabaseForText", requireAll = true)
+        @JvmStatic
+        fun errorTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString()
+            } else if (apiResponse is NetworkResult.Loading) {
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+}
