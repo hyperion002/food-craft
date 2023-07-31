fe96a9920acd8e98a9824cd213b5db1027c25c16
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-01 03:28:07 +0530

    Implement UI

---
+++ app/src/main/java/com/example/foodcraft/ui/fragments/recipes/RecipesFragment.kt
@@ -0,0 +1,114 @@
+package com.example.foodcraft.ui.fragments.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodcraft.adapters.RecipesAdapter
+import com.example.foodcraft.databinding.FragmentRecipesBinding
+import com.example.foodcraft.util.NetworkResult
+import com.example.foodcraft.util.observeOnce
+import com.example.foodcraft.viewmodels.MainViewModel
+import com.example.foodcraft.viewmodels.RecipesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class RecipesFragment : Fragment() {
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
+    private val recipesAdapter by lazy { RecipesAdapter() }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+        recipesViewModel = ViewModelProvider(requireActivity())[RecipesViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
+
+        setUpRecyclerView()
+        readDatabase()
+
+        return binding.root
+    }
+
+    private fun readDatabase() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observe(viewLifecycleOwner) { database ->
+                if (database.isNotEmpty()) {
+                    recipesAdapter.setData(database[0].foodRecipe)
+                    hideShimmerEffect()
+                } else {
+                    requestApiData()
+                }
+            }
+        }
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
+        mainViewModel.recipesResponse.observeOnce(viewLifecycleOwner) { response ->
+            when(response) {
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let {
+                        recipesAdapter.setData(it)
+                    }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    loadDataFromCache()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmerEffect()
+                }
+            }
+        }
+    }
+
+    private fun loadDataFromCache() {
+        lifecycleScope.launch {
+            mainViewModel.readRecipes.observe(viewLifecycleOwner) { database ->
+                recipesAdapter.setData(database[0].foodRecipe)
+            }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        binding.shimmerRecyclerView.adapter = recipesAdapter
+        binding.shimmerRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        binding.shimmerRecyclerView.showShimmer()
+    }
+
+    private fun hideShimmerEffect() {
+        binding.shimmerRecyclerView.hideShimmer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
