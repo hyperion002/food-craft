f57b7b8bfd36a26a705f869e84f35c61cba16ec6
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-01 03:26:09 +0530

    Implement RecipesAdapter.kt

---
+++ app/src/main/java/com/example/foodcraft/adapters/RecipesAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.foodcraft.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodcraft.databinding.RecipesItemBinding
+import com.example.foodcraft.models.FoodRecipe
+import com.example.foodcraft.models.Result
+import com.example.foodcraft.util.RecipesDiffUtil
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>() {
+
+    private var recipes = emptyList<Result>()
+    class RecipesViewHolder(private val binding: RecipesItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result) {
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): RecipesViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesItemBinding.inflate(layoutInflater, parent, false)
+                return RecipesViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesViewHolder {
+        return RecipesViewHolder.from(parent)
+    }
+
+    override fun getItemCount(): Int = recipes.size
+
+    override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
+    }
+
+    fun setData(newData: FoodRecipe) {
+        val recipesDiffUtil = RecipesDiffUtil(recipes, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipes = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
