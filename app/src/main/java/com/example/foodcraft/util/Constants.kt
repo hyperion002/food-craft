eeba10232f3398c4fab4ed44c34440bf417d56f0
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-02 02:47:05 +0530

    Add BottomSheet Constants

--- app/src/main/java/com/example/foodcraft/util/Constants.kt
+++ app/src/main/java/com/example/foodcraft/util/Constants.kt
@@ -3,6 +3,7 @@ package com.example.foodcraft.util
 class Constants {
     companion object {
         const val API_KEY = "2a147263587443da88d176c63803f825"
+//        const val API_KEY = ac3dd11f23ae432a932fd2983d71890f
         const val BASE_URL = "https://api.spoonacular.com"
 
         // API Query Keys
@@ -16,5 +17,16 @@ class Constants {
         // ROOM Database
         const val DATABASE_NAME = "recipes_database"
         const val RECIPES_TABLE = "recipes_table"
+
+        // BottomSheet & Preferences
+        const val DEFAULT_RECIPES_NUMBER = "50"
+        const val DEFAULT_MEAL_TYPE = "main course"
+        const val DEFAULT_DIET_TYPE = "gluten free"
+
+        const val PREFERENCES_NAME = "food preferences"
+        const val PREFERENCES_MEAL_TYPE = "mealType"
+        const val PREFERENCES_MEAL_TYPE_ID = "mealTypeId"
+        const val PREFERENCES_DIET_TYPE = "dealType"
+        const val PREFERENCES_DIET_TYPE_ID = "dealTypeId"
     }
 }
