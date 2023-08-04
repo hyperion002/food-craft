df3e532aa0e2aaedc7989a886b43eb3ea0d76d2a
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-04 12:04:45 +0530

    Add FavouritesEntity

--- app/src/main/java/com/example/foodcraft/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/foodcraft/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.example.foodcraft.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.foodcraft.data.database.entities.FavouritesEntity
 import com.example.foodcraft.data.database.entities.RecipesEntity
 
 @TypeConverters(RecipesTypeConverter::class)
-@Database(entities = [RecipesEntity::class], version = 1, exportSchema = false)
+@Database(entities = [RecipesEntity::class, FavouritesEntity::class], version = 1, exportSchema = false)
 abstract class RecipesDatabase : RoomDatabase() {
 
     abstract fun RecipesDao(): RecipesDao
