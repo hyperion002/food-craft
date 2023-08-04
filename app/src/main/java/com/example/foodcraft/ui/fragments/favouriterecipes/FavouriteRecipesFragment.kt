d74903dece2a459f19c998904fc16efec4c16b4a
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-04 21:22:48 +0530

    Implement FavouriteRecipesFragment

--- app/src/main/java/com/example/foodcraft/ui/fragments/favouriterecipes/FavouriteRecipesFragment.kt
+++ app/src/main/java/com/example/foodcraft/ui/fragments/favouriterecipes/FavouriteRecipesFragment.kt
@@ -5,14 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.foodcraft.R
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodcraft.adapters.FavouriteRecipesAdapter
+import com.example.foodcraft.databinding.FragmentFavouriteRecipesBinding
+import com.example.foodcraft.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavouriteRecipesFragment : Fragment() {
+    private var _binding: FragmentFavouriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+    private val favouriteRecipesAdapter: FavouriteRecipesAdapter by lazy { FavouriteRecipesAdapter() }
+    private val mainVIewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favourite_recipes, container, false)
+    ): View {
+        _binding = FragmentFavouriteRecipesBinding.inflate(inflater, container, false)
+
+        setUpRecyclerView(binding.recyclerViewFavouriteRecipes)
+        mainVIewModel.readFavouriteRecipes.observe(viewLifecycleOwner) { favouritesEntity ->
+            favouriteRecipesAdapter.setData(favouritesEntity)
+        }
+
+        return binding.root
+    }
+
+    private fun setUpRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = favouriteRecipesAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
