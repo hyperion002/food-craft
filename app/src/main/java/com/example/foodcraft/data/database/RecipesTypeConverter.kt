ad291c0effa6d0c1e625f954bf4d5671b16fd227
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-04 12:05:12 +0530

    Add TypeConverters for FavouritesEntity

--- app/src/main/java/com/example/foodcraft/data/database/RecipesTypeConverter.kt
+++ app/src/main/java/com/example/foodcraft/data/database/RecipesTypeConverter.kt
@@ -2,6 +2,7 @@ package com.example.foodcraft.data.database
 
 import androidx.room.TypeConverter
 import com.example.foodcraft.models.FoodRecipe
+import com.example.foodcraft.models.Result
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -20,4 +21,14 @@ class RecipesTypeConverter {
         return gson.fromJson(data, listType)
     }
 
+    @TypeConverter
+    fun resultToString(result: Result): String {
+        return gson.toJson(result)
+    }
+
+    @TypeConverter
+    fun stringToResult(data: String): Result {
+        val listType = object : TypeToken<Result>() {}.type
+        return gson.fromJson(data, listType)
+    }
 }
