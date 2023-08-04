6a2d399bde2fc3dcd5b646a9b8ddb5275ebf8ebd
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-04 12:06:21 +0530

    Create FavouritesEntity DAOs

--- app/src/main/java/com/example/foodcraft/data/LocalDataSource.kt
+++ app/src/main/java/com/example/foodcraft/data/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.foodcraft.data
 
 import com.example.foodcraft.data.database.RecipesDao
+import com.example.foodcraft.data.database.entities.FavouritesEntity
 import com.example.foodcraft.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -13,7 +14,24 @@ class LocalDataSource @Inject constructor(
         recipesDao.insertRecipes(recipesEntity)
     }
 
-    fun readDatabase(): Flow<List<RecipesEntity>> {
+    fun readRecipes(): Flow<List<RecipesEntity>> {
         return recipesDao.readRecipes()
     }
+
+    suspend fun insertFavouriteRecipe(favouritesEntity: FavouritesEntity) {
+        recipesDao.insertFavouriteRecipe(favouritesEntity)
+    }
+
+    fun readFavouriteRecipes(): Flow<List<FavouritesEntity>> {
+        return recipesDao.readFavouriteRecipes()
+    }
+
+    suspend fun deleteFavouriteRecipe(favouritesEntity: FavouritesEntity) {
+        recipesDao.deleteFavouriteRecipes(favouritesEntity)
+    }
+
+    suspend fun deleteAllFavouriteRecipes() {
+        recipesDao.deleteAllFavouriteRecipes()
+    }
+
 }
