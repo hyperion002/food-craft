23d8a8907d5ac907116a77bf4666aaf5a64258a4
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-05 14:12:07 +0530

    Implement FoodJokeBindings

--- app/src/main/java/com/example/foodcraft/bindingadapters/FoodJokeBindings.kt
+++ app/src/main/java/com/example/foodcraft/bindingadapters/FoodJokeBindings.kt
@@ -2,6 +2,7 @@ package com.example.foodcraft.bindingadapters
 
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.foodcraft.data.database.entities.FoodJokeEntity
 import com.example.foodcraft.models.FoodJoke
@@ -54,7 +55,30 @@ class FoodJokeBindings {
                     }
                 }
 
-                null -> {}
+                else -> {}
+            }
+        }
+
+        @BindingAdapter("readApiResponseError", "readDataBaseError", requireAll = true)
+        @JvmStatic
+        fun setErrorViewsVisibility(
+            view: View,
+            apiResponse: NetworkResult<FoodJoke>?,
+            database: List<FoodJokeEntity>?
+        ) {
+            if (database != null) {
+                if (database.isEmpty()) {
+                    view.visibility = View.VISIBLE
+                    if (view is TextView) {
+                        if (apiResponse != null) {
+                            view.text = apiResponse.message.toString()
+                        }
+                    }
+                }
+            }
+
+            if (apiResponse is NetworkResult.Success) {
+                view.visibility = View.INVISIBLE
             }
         }
     }
