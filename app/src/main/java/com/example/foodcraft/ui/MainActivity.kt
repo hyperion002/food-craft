746fb0511bf17f160b954fb71c1dec00024aebf8
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-13 21:38:14 +0530

    Code Refactor

--- app/src/main/java/com/example/foodcraft/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodcraft/ui/MainActivity.kt
@@ -13,8 +13,11 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
