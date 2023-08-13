c6803d9e452e3f3676659a94725a15be667dd81a
Abhishek <93080560+hyperion002@users.noreply.github.com>
2023-08-13 21:01:30 +0530

    Migrate to MenuHost

--- app/src/main/java/com/example/foodcraft/ui/fragments/favouriterecipes/FavouriteRecipesFragment.kt
+++ app/src/main/java/com/example/foodcraft/ui/fragments/favouriterecipes/FavouriteRecipesFragment.kt
@@ -7,8 +7,11 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodcraft.R
@@ -20,6 +23,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FavouriteRecipesFragment : Fragment() {
+
     private var _binding: FragmentFavouriteRecipesBinding? = null
     private val binding get() = _binding!!
 
@@ -36,13 +40,27 @@ class FavouriteRecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavouriteRecipesBinding.inflate(inflater, container, false)
+
         binding.lifecycleOwner = this
         binding.mainViewModel = mainViewModel
         binding.favouriteRecipesAdapter = favouriteRecipesAdapter
 
         setUpRecyclerView(binding.recyclerViewFavouriteRecipes)
 
-        setHasOptionsMenu(true)
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.favourite_recipes_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.delete_all_favourite_recieps) {
+                    mainViewModel.deleteAllFavouriteRecipe()
+                    showSnackBar()
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         return binding.root
     }
@@ -52,29 +70,17 @@ class FavouriteRecipesFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
+    private fun showSnackBar() {
+        Snackbar.make(
+            binding.root,
+            "All recipes removed!",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         favouriteRecipesAdapter.clearContextualActionMode()
         _binding = null
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.favourite_recipes_menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.delete_all_favourite_recieps) {
-            mainViewModel.deleteAllFavouriteRecipe()
-            showSnackBar("All Recipes Removed")
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun showSnackBar(message: String) {
-        Snackbar.make(
-            binding.root,
-            message,
-            Snackbar.LENGTH_SHORT
-        ).show()
     }
 }
